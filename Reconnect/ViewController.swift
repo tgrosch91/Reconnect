c16f39576a571634638a81e5fb13e62bdb5e2028
tgrosch91 <tgrosch91@gmail.com>
2017-01-09 15:57:03 -0600

    CNView thing working

--- Reconnect/ViewController.swift
+++ Reconnect/ViewController.swift
@@ -7,8 +7,17 @@
 //
 
 import UIKit
+import Contacts
+import ContactsUI
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textField: UITextField!
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    var store = CNContactStore()
+    var contacts: [CNContact] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +26,25 @@ class ViewController: UIViewController {
             print(accessGranted)
         })
     }
+    
+    func findContactsWithName(name: String) {
+        AppDelegate.sharedDelegate().checkAccessStatus(completionHandler: { (accessGranted) -> Void in
+            if accessGranted {
+                DispatchQueue.main.async(execute: { () -> Void in
+                    do {
+                        let predicate: NSPredicate = CNContact.predicateForContacts(matchingName: name)
+                        let keysToFetch = [CNContactGivenNameKey, CNContactFamilyNameKey, CNContactBirthdayKey, CNContactViewController.descriptorForRequiredKeys()] as [Any]
+                        self.contacts = try self.store.unifiedContacts(matching: predicate, keysToFetch:keysToFetch as! [CNKeyDescriptor])
+                        self.tableView.reloadData()
+                    }
+                    catch {
+                        print("Unable to refetch the selected contact.")
+                    }
+                })
+            }
+        })
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -24,5 +52,42 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return contacts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let CellIdentifier = "MyCell"
+        let cell = tableView.dequeueReusableCell(withIdentifier: CellIdentifier)
+        cell!.textLabel!.text = contacts[indexPath.row].givenName + " " + contacts[indexPath.row].familyName
+        
+        if let birthday = contacts[indexPath.row].birthday {
+            let formatter = DateFormatter()
+            formatter.dateStyle = DateFormatter.Style.long
+            formatter.timeStyle = .none
+            
+            cell!.detailTextLabel?.text = formatter.string(from: ((birthday as NSDateComponents).date)!)
+        }
+        return cell!
+    }
+    
+}
+
+//MARK: - UITableViewDelegate
+
+extension ViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let controller = CNContactViewController(for: contacts[indexPath.row])
+        controller.contactStore = self.store
+        controller.allowsEditing = false
+        self.navigationController?.pushViewController(controller, animated: true)
+    }
+    
 }
 
