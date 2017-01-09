cd981ce5e1cd846f16441c296fbfe359f8bb4333
tgrosch91 <tgrosch91@gmail.com>
2017-01-09 12:28:23 -0600

    accessing contacts- writing in permission code

--- Reconnect/AppDelegate.swift
+++ Reconnect/AppDelegate.swift
@@ -7,12 +7,39 @@
 //
 
 import UIKit
+import Contacts
+
+
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    var store = CNContactStore()
+    
+    func checkAccessStatus(completionHandler: @escaping (_ accessGranted: Bool) -> Void){
+        let authorizationStatus = CNContactStore.authorizationStatus(for: CNEntityType.contacts)
+        
+        switch authorizationStatus {
+        case .authorized:
+            completionHandler(true)
+        case .denied, .notDetermined:
+            self.store.requestAccess(for: CNEntityType.contacts, completionHandler: { (access, accessError) -> Void in
+                if access {
+                    completionHandler(access)
+                } else {
+                    print("access denied")
+                }
+            })
+        default:
+            completionHandler(false)
+        }
+    }
 
+    class func sharedDelegate() -> AppDelegate {
+        return UIApplication.shared.delegate as! AppDelegate
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
